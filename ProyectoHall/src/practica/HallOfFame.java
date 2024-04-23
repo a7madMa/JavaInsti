bdfac1a66a4b0646a3d883eb660aed8dda23e384
a7madMa <amawaslihabbal@iessonferrer.net>
2024-04-23 19:39:02 +0200

    Dia 23-04-2023

--- ProyectoHall/src/practica/HallOfFame.java
+++ ProyectoHall/src/practica/HallOfFame.java
@@ -1,71 +1,274 @@
 package practica;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class HallOfFame extends JFrame {
 
-	public HallOfFame() {
+	private static final long serialVersionUID = 1L;
 
-		super("Puntuación");
-		setSize(390, 550);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(new FlowLayout());
+	private JPanel contentPane;
 
-		JLabel lblTexto = new JLabel("*** Hall of Fame ***");
+	
+	 
 
-		JList<String> lstPuntos = new JList<>();
+	JList lstContenido;
 
-		JScrollPane scrPuntos = new JScrollPane(lstPuntos);
-		scrPuntos.setPreferredSize(new Dimension(345, 450));
+	DefaultListModel model;
 
-		JButton btnNuevo = new JButton("Nuevo");
-		JButton btnBorrar = new JButton("Borrar");
+	final String NOMBRE_ARCHIVO = "ficheros/jugadores.txt";
 
-		add(lblTexto);
-		add(scrPuntos);
-		add(btnNuevo);
-		add(btnBorrar);
+	public static void main(String[] args) {
 
-		setLocationRelativeTo(null);
-		setVisible(true);
+		EventQueue.invokeLater(new Runnable() {
 
-		btnNuevo.addActionListener(new ActionListener() {
+			public void run() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
+				try {
 
-				String eleccion = JOptionPane.showInputDialog(null, "¿Nombre del jugador?", "Nuevo",
-						JOptionPane.QUESTION_MESSAGE);
-				if (eleccion != null && eleccion.trim().equals("")) {
-					JOptionPane.showInputDialog(null, "¿Puntuación del jugador?", "Nuevo",
-							JOptionPane.QUESTION_MESSAGE);
+					HallOfFame frame = new HallOfFame();
+
+					frame.setVisible(true);
+
+				} catch (Exception e) {
+
+					e.printStackTrace();
+
 				}
 
 			}
+
 		});
+
+	}
+
+	
+
+	public HallOfFame() {
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setBounds(100, 100, 293, 404);
+
+		contentPane = new JPanel();
+
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+
+		contentPane.setLayout(null);
+
+		JLabel lblTitulo = new JLabel("*** Hall of Fame ***");
+
+		lblTitulo.setBounds(62, 0, 131, 15);
+
+		contentPane.add(lblTitulo);
+
+		model = new DefaultListModel();
+
+		lstContenido = new JList(model);
+		lstContenido.setBounds(12, 27, 258, 277);
+
+		contentPane.add(lstContenido);
+
+		JButton btnNuevo = new JButton("Nuevo");
+
+		btnNuevo.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+
+				nuevoJugador();
+
+			}
+
+		});
+
+		btnNuevo.setBounds(56, 316, 78, 25);
+
+		contentPane.add(btnNuevo);
+
+		JButton btnBorrar = new JButton("Borrar");
 
 		btnBorrar.addActionListener(new ActionListener() {
 
 			@Override
+
 			public void actionPerformed(ActionEvent e) {
 
-				JOptionPane.showConfirmDialog(null, "¿Seguro que quieres borrar todos los datos de jugadores?",
-						"Advertencia", JOptionPane.QUESTION_MESSAGE);
+				borrarArchivo();
+
 			}
+
 		});
+
+		btnBorrar.setBounds(146, 316, 87, 25);
+
+		contentPane.add(btnBorrar);
+
+		comprobacion();
+		cargarJugadores();
+
 	}
 
-	public static void main(String[] args) {
-		new HallOfFame();
+	public class Jugador {
+
+		private String nombre;
+		private int puntuacion;
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public int getPuntuacion() {
+			return puntuacion;
+		}
+
+		public void setPuntuacion(int puntuacion) {
+			this.puntuacion = puntuacion;
+		}
+
+		public Jugador(String nombre, int puntuacion) {
+			super();
+			this.nombre = nombre;
+			this.puntuacion = puntuacion;
+		}
+
+	}
+
+	
+
+	private void nuevoJugador() {
+
+		int puntuacion;
+
+		String nombre = JOptionPane.showInputDialog(null, "¿Nombre del jugador?", "Nuevo",
+
+				JOptionPane.QUESTION_MESSAGE);
+
+		if (nombre == null || nombre.trim().equals("")) {
+
+			return;
+
+		}
+
+		String puntuacionString = JOptionPane.showInputDialog(null, "¿Puntuación del jugador?", "Nuevo",
+
+				JOptionPane.QUESTION_MESSAGE);
+		puntuacion = Integer.parseInt(puntuacionString);
+		model.addElement(nombre + "..." + puntuacion + " puntos.\n");
+		Jugador nuevoJugador = new Jugador(nombre, puntuacion);
+		guardarJugador(nuevoJugador);
+
+	}
+
+	
+
+
+
+	public void guardarJugador(Jugador jugador) {
+		try {
+
+			FileWriter archivo = new FileWriter(NOMBRE_ARCHIVO, true);
+
+			BufferedWriter buffer = new BufferedWriter(archivo);
+
+			buffer.write(jugador.getNombre() + "," + jugador.getPuntuacion() + "\n");
+
+			buffer.close();
+
+		} catch (IOException e) {
+
+			JOptionPane.showConfirmDialog(null, "Error de E/S", "Error", JOptionPane.CLOSED_OPTION,
+					JOptionPane.WARNING_MESSAGE);
+		}
+	}
+
+	
+	public void cargarJugadores() {
+		try {
+			BufferedReader bufferLectura = new BufferedReader(new FileReader(NOMBRE_ARCHIVO));
+
+			String fila = bufferLectura.readLine();
+
+			while (fila != null) {
+				String[] textoSeparado = fila.split(",");
+				model.addElement(textoSeparado[0] + "..." + textoSeparado[1] + " puntos.");
+				fila = bufferLectura.readLine();
+
+			}
+
+			bufferLectura.close();
+		} catch (FileNotFoundException e) {
+			JOptionPane.showConfirmDialog(null, "No se ha encontrado el archivo", "Error", JOptionPane.CLOSED_OPTION,
+					JOptionPane.WARNING_MESSAGE);
+		} catch (IOException e) {
+			JOptionPane.showConfirmDialog(null, "Error de E/S", "Error", JOptionPane.CLOSED_OPTION,
+					JOptionPane.WARNING_MESSAGE);
+		}
+	}
+
+	
+
+	public void borrarArchivo() {
+
+		int borrar = JOptionPane.showConfirmDialog(null, "¿Seguro que quieres borrar todos los datos de jugadores?",
+
+				"Advertencia", JOptionPane.CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+		if (borrar == JOptionPane.CANCEL_OPTION) {
+
+			JOptionPane.showConfirmDialog(null, "Operación cancelada. No se ha borrado ningún dato.", "Mensaje",
+
+					JOptionPane.CLOSED_OPTION, JOptionPane.INFORMATION_MESSAGE);
+
+		}
+
+		model.clear();
+
+		File archivoCreado = new File(NOMBRE_ARCHIVO);
+
+		archivoCreado.delete();
+
+	}
+
+	public void comprobacion() {
+		File crearArchivo = new File(NOMBRE_ARCHIVO);
+		BufferedWriter buffer;
+		if (crearArchivo.exists()) {
+			return;
+		} else {
+			try {
+				buffer = new BufferedWriter(new FileWriter(crearArchivo));
+				JOptionPane.showConfirmDialog(null,
+						"No se han encontrado datos guardados de jugadores. Se creará un archivo nuevo.", "Mensaje",
+						JOptionPane.CLOSED_OPTION, JOptionPane.INFORMATION_MESSAGE);
+
+			} catch (IOException e) {
+				JOptionPane.showConfirmDialog(null, "Error de E/S", "Error", JOptionPane.CLOSED_OPTION,
+						JOptionPane.WARNING_MESSAGE);
+			}
+		}
+
 	}
 }
---
+++ Tema8/src/notasIncompleto/Libreta.java
@@ -0,0 +1,80 @@
+package notasIncompleto;
+
+public class Libreta {
+
+	private final String NOMBRE_ARCHIVO = "ficheros/notas.txt";
+	private final int MAX_NOTAS = 1000;
+
+	private Nota[] notas;
+	private int numNotas;
+
+	public Libreta() {
+		notas = new Nota[MAX_NOTAS];
+		numNotas = 0;
+		leerNotas();
+	}
+
+	public void addNota(Nota nota) {
+
+		/*
+		 * TODO: Añadir después de la última nota del array la nota que se nos pasa como
+		 * parámetro. Utilizar el valor de la variable numNotas. Actualizar después el
+		 * valor de numNotas. En caso de que hayamos llegado al máximo de notas, no
+		 * hacer nada.
+		 */
+
+	}
+
+	public void setNota(int posicion, Nota nota) {
+
+		notas[posicion] = nota;
+
+	}
+
+	public Nota getNota(int posicion) {
+
+		return notas[posicion];
+
+	}
+
+	public void delNota(int posicion) {
+
+		/*
+		 * TODO: Eliminar el elemento del array notas[] que ocupa la posición indicada.
+		 * Para ello, mueve hacia arriba todos los elementos que vengan a continuación
+		 * una posición, y actualiza la variable numNotas.
+		 */
+
+	}
+
+	public void leerNotas() {
+
+		/*
+		 * TODO: Leer todas las filas del archivo, y rellenar el array notas[]. Si no se
+		 * encuentra el archivo, hacer que se muestre el mensaje indicado en el
+		 * enunciado de la práctica. Si se produce otro tipo de excepción, mostrar un
+		 * JOptionPane explicándolo.
+		 * 
+		 * La información de cada nota está guardada en dos líneas de texto, una para el
+		 * título y otra para la descripción. Puedes usar el método split() para separar
+		 * los datos de los comentarios del archivo.
+		 */
+
+	}
+
+	public void guardarNotas() {
+
+		/*
+		 * TODO: Guardar las notas del array notas[] en el archivo "notas.txt". El
+		 * formato en que se guardarán debe ser el que aparece en el enunciado de la
+		 * práctica. Si se produce una excepción, se mostrará el error que aparece en el
+		 * enunciado.
+		 */
+
+	}
+
+	public int getNumNotas() {
+		return numNotas;
+	}
+
+}
